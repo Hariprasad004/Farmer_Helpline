850e9e1bd811c09e54708a39ca0fd9756e866562
Bhavya0029 <bhavyashreepoojary99@gmail.com>
2020-04-26 12:52:55 +0530

    nooo message he know wt to doo

---
+++ app/src/main/java/com/example/myteam/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.myteam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+}
