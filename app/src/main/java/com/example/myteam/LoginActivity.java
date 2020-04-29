ddb4a8c5dc414b5de36afc23ad24df2d2438ff77
Bhavya0029 <bhavyashreepoojary99@gmail.com>
2020-04-29 12:08:59 +0530

    By Bhavya

--- app/src/main/java/com/example/myteam/LoginActivity.java
+++ app/src/main/java/com/example/myteam/LoginActivity.java
@@ -3,15 +3,18 @@ package com.example.myteam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Window;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try{
+            this.getActionBar().hide();
+        }catch (Exception e) {
+
+        }
         setContentView(R.layout.activity_login);
-
     }
-
 }
