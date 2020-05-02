107f64d98e65a73c43b0cc349c2cb8df9759f6f5
Bhavya0029 <bhavyashreepoojary99@gmail.com>
2020-05-02 18:44:13 +0530

    define frget pass

--- app/src/main/java/com/example/myteam/forgetpass.java
+++ app/src/main/java/com/example/myteam/forgetpass.java
@@ -3,12 +3,18 @@ package com.example.myteam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class forgetpass extends AppCompatActivity {
+    private EditText emailenter;
+    private Button resetpass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgetpass);
+        emailenter=(EditText)findViewById(R.id.emailreset);
+        resetpass=(Button)findViewById(R.id.login);
     }
 }
