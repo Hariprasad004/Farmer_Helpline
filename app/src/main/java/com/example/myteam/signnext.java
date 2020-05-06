099dc571926876a71c72d6e51500afe9e7307e3e
Hariprasad004 <57026806+Hariprasad004@users.noreply.github.com>
2020-05-06 22:03:53 +0530

    Dont touch manifest.xml

--- app/src/main/java/com/example/myteam/signnext.java
+++ app/src/main/java/com/example/myteam/signnext.java
@@ -18,7 +18,7 @@ public class signnext extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_sign);
 
         requestSMSPermission();
 
