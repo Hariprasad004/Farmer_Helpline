02475c2583b3db668c7a467432d2314f5318811b
sankethbangera27 <sankethbangera27@gmail.com>
2020-06-08 11:09:20 +0530

    country code

--- app/src/main/java/com/example/myteam/phone.java
+++ app/src/main/java/com/example/myteam/phone.java
@@ -7,15 +7,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.hbb20.CountryCodePicker;
+
 public class phone extends AppCompatActivity {
     private EditText phonenum;
     private Button otp;
+    private CountryCodePicker ccp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone);
-        phonenum=(EditText)findViewById(R.id.num);
+        phonenum=(EditText)findViewById(R.id.phoneText);
         otp=(Button)findViewById(R.id.otpnum);
+        init();
+
 
 
         otp.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +29,26 @@ public class phone extends AppCompatActivity {
                 String phonenumber = phonenum.getText().toString().trim();
             }
         });
+    }
+
+    private void init() {
+        ccp=findViewById(R.id.ccp);
+        phonenum=findViewById(R.id.phoneText);
+        otp=findViewById(R.id.otpnum);
+
+        otp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                //set text
+                getNumber();
+            }
+        });
+
+
+    }
+
+    private void getNumber() {
+
+        String fullNumber=ccp.getFullNumber()+phonenum.getText().toString();
+
     }
 }
