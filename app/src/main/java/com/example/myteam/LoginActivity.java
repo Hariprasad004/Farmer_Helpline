dca4da3a0f5ca84b162fa9da3a86d4ccb7510020
Hariprasad004 <57026806+Hariprasad004@users.noreply.github.com>
2020-06-11 12:00:41 +0530

    Sign up and login

--- app/src/main/java/com/example/myteam/LoginActivity.java
+++ app/src/main/java/com/example/myteam/LoginActivity.java
@@ -1,13 +1,22 @@
 package com.example.myteam;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText emailenter;
@@ -19,24 +28,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        try {
-            this.getActionBar().hide();
-        } catch (Exception e) {
+        //try {
+            //this.getActionBar().hide();
+        //} catch (Exception e) {
 
-        }
+        //}
         setContentView(R.layout.activity_login);
         emailenter = (EditText) findViewById(R.id.email);
         pass = (EditText) findViewById(R.id.passwrd);
         login = (Button) findViewById(R.id.loginid);
         ftpass = (TextView) findViewById(R.id.fgtpass);
 
+        final FirebaseAuth fAuth = FirebaseAuth.getInstance();
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = emailenter.getText().toString().trim();
-                String password = pass.getText().toString().trim();
-                String forget = ftpass.getText().toString().trim();
                 //Initiatializw progress dailogue
                 progressDialog = new ProgressDialog(LoginActivity.this);
                 //show dialog
@@ -47,14 +54,50 @@ public class LoginActivity extends AppCompatActivity {
                 progressDialog.getWindow().setBackgroundDrawableResource(
                         android.R.color.transparent
                 );
+                String email = emailenter.getText().toString().trim();
+                String password = pass.getText().toString().trim();
+                String forget = ftpass.getText().toString().trim();
+                if (TextUtils.isEmpty((email))) {
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this, "Please enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty((password))) {
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this, "Please enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(fAuth.getCurrentUser() == null) {
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this, "User doesn't exists", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            if(fAuth.getCurrentUser().isEmailVerified()){
+                                Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(LoginActivity.this, submit_info.class);
+                                progressDialog.dismiss();
+                                startActivity(intent);
+                                LoginActivity.this.finish();
+                            }
+                            else{
+                                progressDialog.dismiss();
+                                Toast.makeText(LoginActivity.this, "Please verify your email address", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                        else{
+                            progressDialog.dismiss();
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
-        @Override
-        public void onBackPressed() {
-            //dismiss progress dialog
-            progressDialog.dismiss();
-        }
+}
 
-    }
 
--- app/src/main/java/com/example/myteam/signActivity.java
+++ app/src/main/java/com/example/myteam/signActivity.java
@@ -7,6 +7,8 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,9 +17,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class signActivity extends AppCompatActivity {
 private EditText email_txt;
@@ -36,8 +42,8 @@ ProgressDialog progressDialog;
         password_txt = (EditText) findViewById(R.id.pass);
         confirmpass = (EditText) findViewById(R.id.confirm);
         signup = (Button) findViewById(R.id.signup);
-        cphone=(Button)findViewById(R.id.cnphone);
-        al_login=(TextView)findViewById(R.id.allrdy_login);
+        cphone = (Button) findViewById(R.id.cnphone);
+        al_login = (TextView) findViewById(R.id.allrdy_login);
 
         final FirebaseAuth fAuth = FirebaseAuth.getInstance();
 
@@ -61,6 +67,11 @@ ProgressDialog progressDialog;
                     Toast.makeText(signActivity.this, "Please enter Email", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    progressDialog.dismiss();
+                    Toast.makeText(signActivity.this, "Enter the valid email address", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 if (TextUtils.isEmpty((password))) {
                     progressDialog.dismiss();
                     Toast.makeText(signActivity.this, "Please enter Password", Toast.LENGTH_SHORT).show();
@@ -71,30 +82,45 @@ ProgressDialog progressDialog;
                     Toast.makeText(signActivity.this, "Please enter Confirm Password", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if (password.length() < 6){
+                if (password.length() < 6) {
                     progressDialog.dismiss();
                     Toast.makeText(signActivity.this, "Password length should be greater than 6", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                /*if(fAuth.getCurrentUser() != null){
+                    progressDialog.dismiss();
+                    Toast.makeText(signActivity.this, "User already exists", Toast.LENGTH_SHORT).show();
+                    return;
+                }*/
                 if (password.equals(confpass)) {
                     fAuth.createUserWithEmailAndPassword(email, password)
                             .addOnCompleteListener(signActivity.this, new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
-                                        Toast.makeText(signActivity.this, "User successfully registered", Toast.LENGTH_SHORT).show();
-                                        Intent intent =new Intent(signActivity.this, MainActivity.class);
-                                        startActivity(intent);
-                                        onBackPressed();
-                                        signActivity.this.finish();
+                                        fAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if(task.isSuccessful()) {
+                                                    progressDialog.dismiss();
+                                                    Toast.makeText(signActivity.this, "User successfully registered, Please verify your email", Toast.LENGTH_SHORT).show();
+                                                    Intent intent = new Intent(signActivity.this, LoginActivity.class);
+                                                    startActivity(intent);
+                                                    signActivity.this.finish();
+                                                }
+                                                else{
+                                                    progressDialog.dismiss();
+                                                    Toast.makeText(signActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
                                     } else {
                                         progressDialog.dismiss();
-                                        Toast.makeText(signActivity.this, "Aunthentication failed", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(signActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                                     }
                                 }
                             });
-                }
-                else {
+                } else {
                     Toast.makeText(signActivity.this, "Password and confirm password doesn't match", Toast.LENGTH_SHORT).show();
                     progressDialog.dismiss();
                 }
@@ -103,7 +129,7 @@ ProgressDialog progressDialog;
         cphone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(signActivity.this,phone.class);
+                Intent intent = new Intent(signActivity.this, MainActivity.class);
                 startActivity(intent);
                 signActivity.this.finish();
             }
@@ -111,7 +137,7 @@ ProgressDialog progressDialog;
         al_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(signActivity.this,LoginActivity.class);
+                Intent intent = new Intent(signActivity.this, LoginActivity.class);
                 startActivity(intent);
                 signActivity.this.finish();
             }
