7797ae0655adef0fe4b90983e72943be71cdf7f6
Hariprasad004 <57026806+Hariprasad004@users.noreply.github.com>
2020-06-20 14:56:54 +0530

    Till Storing the details in firestore(Includes authentication and access user's location)

--- app/src/main/java/com/example/myteam/submit_info.java
+++ app/src/main/java/com/example/myteam/submit_info.java
@@ -1,26 +1,18 @@
 package com.example.myteam;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
-import android.annotation.SuppressLint;
-import android.app.Dialog;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
-import android.location.Location;
 import android.location.LocationManager;
-import android.media.audiofx.BassBoost;
 import android.os.Bundle;
+import android.os.Looper;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,20 +20,30 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.hbb20.CountryCodePicker;
 
-import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 public class submit_info extends AppCompatActivity {
     private static final int MY_PERMISSION_REQUEST_ACCESS_LOCATION = 1;
+    public static final String TAG = "TAG";
+    private static final int GPS_REQUEST_CODE = 9003;
     private EditText Name;
     private EditText number;
     private EditText Age;
@@ -50,8 +52,12 @@ public class submit_info extends AppCompatActivity {
     private RadioButton radButton;
     private CountryCodePicker ccode;
     private Button submit;
-    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationManager locationManager;
     ProgressDialog progressDialog;
+    String userId;
+    Double final_latitude = 0.0, final_longitude = 0.0;
+    boolean addr = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,17 +67,14 @@ public class submit_info extends AppCompatActivity {
         Age = (EditText) findViewById(R.id.age);
         address = (EditText) findViewById(R.id.add);
         submit = (Button) findViewById(R.id.sub);
-        ccode=findViewById(R.id.ccp);
+        ccode = findViewById(R.id.ccp);
         radioGroup = findViewById(R.id.radbtn);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         getSupportActionBar().setTitle("Details of the User");
-
-        //Initialize fusedLocationProviderClient
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        fetchLocation();
-
+        isGPSEnabled();
+        requestPermission();
         final FirebaseAuth fAuth = FirebaseAuth.getInstance();
-
-
+        final FirebaseFirestore fStore = FirebaseFirestore.getInstance();
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -84,54 +87,130 @@ public class submit_info extends AppCompatActivity {
                 progressDialog.getWindow().setBackgroundDrawableResource(
                         android.R.color.transparent
                 );
+                int radioId = radioGroup.getCheckedRadioButtonId();
                 String NAme = Name.getText().toString().trim();
                 String num = number.getText().toString().trim();
                 String person_age = Age.getText().toString().trim();
-                String person_adress = address.getText().toString().trim();
+                String person_address = address.getText().toString().trim();
                 String code = ccode.getSelectedCountryCode();
-                String PNum = "+"+code+num;
+                String PNum = "+" + code + num;
+                userId = fAuth.getCurrentUser().getUid();
+                if (NAme.isEmpty()) {
+                    progressDialog.dismiss();
+                    Name.setError("Enter the name");
+                    return;
+                }
+                if (num.length() != 10) {
+                    progressDialog.dismiss();
+                    number.setError("Enter the valid phone number");
+                    return;
+                } else if (person_age.isEmpty()) {
+                    progressDialog.dismiss();
+                    Age.setError("Enter the age");
+                    return;
+                } else if (radioId == -1) {
+                    progressDialog.dismiss();
+                    ;
+                    Toast.makeText(submit_info.this, "Select whether lender or farmer", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (person_address.isEmpty()) {
+                    progressDialog.dismiss();
+                    address.setError("Enter the address with pincode");
+                    return;
+                } else {
+                    addr = false;
+                    requestPermission();
+                    radButton = findViewById(radioId);
+                    String farmlend = radButton.getText().toString();
+                    DocumentReference documentReference = fStore.collection("User").document(userId);
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("Name", NAme);
+                    user.put("Phone_Number", PNum);
+                    user.put("Age", person_age);
+                    user.put("FarmLend", farmlend);
+                    user.put("Latitude", final_latitude);
+                    user.put("Longitude", final_longitude);
+                    user.put("Address", person_address);
+                    documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            progressDialog.dismiss();
+                            Toast.makeText(submit_info.this, "User profile is created", Toast.LENGTH_SHORT).show();
+                            Log.d(TAG, "User profile is created");
+                            Intent intent = new Intent(submit_info.this, MainActivity.class);
+                            startActivity(intent);
+                            submit_info.this.finish();
+                        }
+                    });
+                }
 
             }
         });
     }
 
-    private void fetchLocation() {
-        //If permission is not granted
-        if (ContextCompat.checkSelfPermission(
-                submit_info.this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
-                submit_info.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(submit_info.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
-                        Manifest.permission.ACCESS_FINE_LOCATION},
-                MY_PERMISSION_REQUEST_ACCESS_LOCATION);
-        }else{
-            //If permission is already granted
-            fusedLocationProviderClient.getLastLocation()
-                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            // Got last known location. In some rare situations this can be null.
-                            if (location != null) {
-                                // Logic to handle location object
-                                Double latitude = location.getLatitude();
-                                Double longitude = location.getLongitude();
-                                getCompleteAddress(latitude, longitude);
 
-                            }
-                        }
-                    });
-        }
-
+    //-----------Location Access------------
+    private void requestPermission() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(submit_info.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_REQUEST_ACCESS_LOCATION);
+        } else
+            getCurrentLocation();
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == MY_PERMISSION_REQUEST_ACCESS_LOCATION){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                fetchLocation();
-            }else{
-
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_PERMISSION_REQUEST_ACCESS_LOCATION && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+            } else {
+                requestPermission();
             }
+        }
+    }
+
+    private void getCurrentLocation() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(2000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+        }
+        LocationServices.getFusedLocationProviderClient(submit_info.this).requestLocationUpdates(locationRequest, new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                LocationServices.getFusedLocationProviderClient(submit_info.this).removeLocationUpdates(this);
+                if(locationResult!= null && locationResult.getLocations().size() > 0){
+                    int latestlocationindex = locationResult.getLocations().size() - 1;
+                    final_longitude = locationResult.getLocations().get(latestlocationindex).getLongitude();
+                    final_latitude = locationResult.getLocations().get(latestlocationindex).getLatitude();
+                    Log.d(TAG, String.valueOf(final_latitude));
+                    Log.d(TAG, String.valueOf(final_longitude));
+                    if(addr)
+                    getCompleteAddress(final_latitude, final_longitude);
+                }
+            }
+        }, Looper.getMainLooper());
+
+    }
+
+    private void isGPSEnabled() {
+        boolean providerEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (providerEnabled) {
+            return;
+        } else {
+            AlertDialog alertDialog = new AlertDialog.Builder(this)
+                    .setTitle("GPS Permission")
+                    .setMessage("GPS is required for this app to work. Please enable GPS.")
+                    .setPositiveButton("OK", ((dialogInterface, i) -> {
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivityForResult(intent, GPS_REQUEST_CODE);
+                    }))
+                    .setCancelable(false)
+                    .show();
         }
     }
 
@@ -151,7 +230,7 @@ public class submit_info extends AppCompatActivity {
             }
         }
         catch (Exception e){
-            Toast.makeText(this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
 
     }
