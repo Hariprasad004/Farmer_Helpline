935583dbe6da285c0b9eb8803c1180f5603ff3ab
sankethbangera27 <sankethbangera27@gmail.com>
2020-08-20 23:10:45 +0530

    edit

---
+++ app/src/main/java/com/example/myteam/model.java
@@ -0,0 +1,47 @@
+package com.example.myteam;
+
+public class model {
+    String Name,Address,Image;
+    int Age,Phone;
+    public model(){}
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public void setImage(String image) {
+        Image = image;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public int getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(int phone) {
+        Phone = phone;
+    }
+}
